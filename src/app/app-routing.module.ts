4f66423036c02f0fccfa47cdc2685c6905476566
Elnatan Emanuel Lima Santos <elnatan.santos@neomed.tech>
2022-04-20 00:47:25 -0300

    create ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { UiComponent } from './components/ui/ui.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  component: UiComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
